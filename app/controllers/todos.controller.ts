66e3de66945614a7ab6f622690570989759b4db6
irumvanselme <andesanselme@gmail.com>
2021-05-27 17:58:45 +0200

    Try to minimize the functionalities

--- app/controllers/todos.controller.ts
+++ app/controllers/todos.controller.ts
@@ -12,17 +12,6 @@ class TodosController {
         }
     }
 
-    async get_one(req: Request, res: Response) {
-        try {
-            const todo = await Todo.findById(req.params.id);
-            if (!todo)
-                return res.status(404).send({ message: "Todo not found" });
-            return res.send(todo);
-        } catch (e) {
-            return res.status(500).send(e);
-        }
-    }
-
     async create(req: Request, res: Response) {
         try {
             const valid = validate(req.body);
@@ -38,40 +27,6 @@ class TodosController {
             return res
                 .status(400)
                 .send({ message: "Failed to create a new todo" });
-        } catch (e) {
-            return res.status(500).send(e);
-        }
-    }
-
-    async edit(req: Request, res: Response) {
-        try {
-            const valid = validate(req.body);
-            if (valid.fails(undefined))
-                return res.status(400).send(valid.errors.all());
-
-            const updatedTodo = await Todo.findByIdAndUpdate(
-                req.params.id,
-                req.body,
-                { new: true }
-            );
-            if (!updatedTodo)
-                return res
-                    .status(500)
-                    .send({ message: "Failed to update the todo " });
-            return res.send(updatedTodo);
-        } catch (e) {
-            return res.status(500).send(e);
-        }
-    }
-
-    async delete(req: Request, res: Response) {
-        try {
-            const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
-            if (!deletedTodo)
-                return res
-                    .status(500)
-                    .send({ message: "Failed to delete the todo " });
-            return res.send(deletedTodo);
         } catch (e) {
             return res.status(500).send(e);
         }
--- app/routes/todos.routes.ts
+++ app/routes/todos.routes.ts
@@ -4,9 +4,6 @@ import TodoController from "../controllers/todos.controller";
 const router = Router();
 
 router.get("/", TodoController.get_all);
-router.get("/:id", TodoController.get_one);
 router.post("/", TodoController.create);
-router.put("/:id", TodoController.edit);
-router.delete("/:id", TodoController.delete);
 
 export default router;
