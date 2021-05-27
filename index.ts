b8045a7eb11b6c3475fb095eeb712725b9fc01da
irumvanselme <andesanselme@gmail.com>
2021-05-27 15:59:50 +0200

    Finish starting typescript app

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
+import { config } from "dotenv"
+config()
+
 import express from "express"
 
 const app: express.Application = express()
 
-app.listen(2000, () => {
-    console.log("[VEPA]");
-})
+app.listen(process.env.PORT, () =>
+    console.log(
+        `[${new Date().toUTCString()}] Development Server 1  (http://localhost:${
+            process.env.PORT
+        }) started`
+    )
+);
