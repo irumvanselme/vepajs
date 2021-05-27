66f93ffed85757635e6cd479c3a4039d8eef257e
irumvanselme <andesanselme@gmail.com>
2021-05-27 16:07:35 +0200

    Add an entry point

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import { config } from "dotenv"
 config()
 
-import express from "express"
+import express from "express";
+import { join, resolve } from "path";
 
 const app: express.Application = express()
+
+app.get("/", (req: express.Request, res: express.Response)=> {
+    res.sendFile(join(resolve(), "/app/views/welcome.html"))
+})
 
 app.listen(process.env.PORT, () =>
     console.log(
