1727d029e5054ab7930e21c3f824bbad0602e109
irumvanselme <andesanselme@gmail.com>
2021-05-27 15:55:12 +0200

    Go to initial stage

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import express from "express"
 const app: express.Application = express()
 
 app.listen(2000, () => {
-    console.log("Something is working 1 h ubuntu hola and I like it and it is looking good");
+    console.log("[VEPA]");
 })
